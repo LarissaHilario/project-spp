590e897ad1d8dc62dac5f173d716216034b2db72
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2023-03-24 09:01:43 -0600

    add login

--- src/Pages/Login/Components/Form.jsx
+++ src/Pages/Login/Components/Form.jsx
@@ -30,7 +30,7 @@ export const Form = () => {
                         <h4>¿OLVIDASTE TU CONTRASEÑA?</h4>
                     </div>
                     <div className="btnIniciar">
-                        <button>Iniciar Sesión</button>
+                        <button>Iniciar Sesió</button>
                     </div>
                 </div>
                 <div className="container">
