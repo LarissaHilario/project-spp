dd8f2fd8fac8284abdce22c7bdce5398e3e72397
Larissa Hilario Clemente <larihid1@gmail.com>
2023-03-23 19:54:18 -0600

    add themes

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+const Home=()=>{
+
+    return(
+    <div className="App">
+      <button class="btn btn-primary">Button</button>
+    </div>
+    )
+}
+
+export default Home
---
+++ themes/Theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material';
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: '#5C3DA4',
+    },
+    secondary: {
+        main: '#B89CF9',
+      },
+    third: {
+        main: '#FE92A1',
+      },
+  },
+});
+
+export default Theme;
