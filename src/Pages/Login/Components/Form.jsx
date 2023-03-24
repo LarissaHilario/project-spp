78839d5cad84a0403ed6a8ee28ba270b54ecd918
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2023-03-24 11:08:54 -0600

    modified login

--- src/Pages/Login/Components/Form.jsx
+++ src/Pages/Login/Components/Form.jsx
@@ -1,9 +1,21 @@
 import '../../Login/Components/Form.css'
 
-/* import FilledInput from '@mui/material/FilledInput';
-import IconButton from '@mui/material/IconButton'; */
+import * as React from 'react';
+import Input from '@mui/material/Input';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Visibility from '@mui/icons-material/Visibility';
 
 export const Form = () => {
+
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
 
     return (
         <>
@@ -23,8 +35,31 @@ export const Form = () => {
                     <div className="inputs">
                         <input type="text" placeholder='Correo Electrónico' />
                     </div>
-                    <div className="inputs">
-                        <input type="password" placeholder='contraseña' />
+                    <div className="inputs2">
+                        <Input
+                            placeholder='CONTRASEÑA'
+                            id="standard-adornment-password"
+                            type={showPassword ? 'text' : 'password'}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                    >
+                                        {showPassword ? <Visibility />:<VisibilityOff />  }
+                                    </IconButton>
+                                </InputAdornment>
+                            } 
+                            sx={{
+                                background: '#D6C6FC',
+                                borderRadius: '10px',
+                                width: '350px',
+                                height: '60px',
+                                fontFamily: 'Unica one',
+                            }}
+                        />
+                       
                     </div>
                     <div className="olvi">
                         <h4>¿OLVIDASTE TU CONTRASEÑA?</h4>
